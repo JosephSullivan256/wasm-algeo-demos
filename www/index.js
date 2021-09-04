f38dc1c813fb7f12fe2b31fc31f528230258b587
Joseph Sullivan <josephsullivan256@gmail.com>
2021-09-04 12:01:32 -0700

    changed camera so that z is up

--- www/index.js
+++ www/index.js
@@ -7,29 +7,36 @@ class Demo {
 	constructor() {
 		this.canvas = document.getElementById('canvas');
 		this.renderer = new THREE.WebGLRenderer({canvas});
+		this.renderer.setClearColor( 0xffffff, 1 );
 	
 		const fov = 75;
 		const aspect = this.canvas.width/this.canvas.height;
 		const near = 0.1;
 		const far = 5;
 		this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-		this.camera.position.z = 2;
+		// change coordinate system good
+		this.camera.up.set(0,0,1);
+		this.camera.position.y = -2;
 		
 		this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-		this.controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+		// an animation loop is required when either damping or auto-rotation are enabled
+		this.controls.enableDamping = true;
 		this.controls.dampingFactor = 0.05;
 	
 		this.scene = new THREE.Scene();
-	
-		const boxWidth = 1;
-		const boxHeight = 1;
-		const boxDepth = 1;
-		const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-	
-		const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
-	
-		this.cube = new THREE.Mesh(geometry, material);
-		this.scene.add(this.cube);
+
+		const iv = new THREE.Vector3(1, 0, 0);
+		const jv = new THREE.Vector3(0, 1, 0);
+		const kv = new THREE.Vector3(0, 0, 1);
+
+		const origin = new THREE.Vector3( 0, 0, 0 );
+		const length = 1;
+		const headLength = 0.05;
+		const headWidth = 0.03;
+
+		this.scene.add(new THREE.ArrowHelper(iv, origin, length, 0xff0000, headLength, headWidth));
+		this.scene.add(new THREE.ArrowHelper(jv, origin, length, 0x00ff00, headLength, headWidth));
+		this.scene.add(new THREE.ArrowHelper(kv, origin, length, 0x0000ff, headLength, headWidth));
 	
 		this.step();
 	}
@@ -43,7 +50,7 @@ class Demo {
 }
 
 function main() {
-	wasm.greet();
+	//wasm.greet();
 	new Demo();
 }
 
