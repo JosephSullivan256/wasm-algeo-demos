3fbd94f49044cc10c075c484ddb61399a2d816e0
Joseph Sullivan <josephsullivan256@gmail.com>
2021-09-06 01:40:12 -0700

    did some customization, began work on actually having the javascript do something, created Context type

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,29 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 //     fn alert(s: &str);
 // }
 
+// impl IntoWasmAbi for Mat<f32> {
+//     type Abi;
+
+//     fn into_abi(self) -> Self::Abi {
+//         todo!()
+//     }
+// }
+
+// #[wasm_bindgen]
+// pub fn mat() -> Mat<f32>{
+//     Mat::new(2, 2, vec![
+//         1.0, 0.0,
+//         3.0, 1.0,
+//     ])
+// }
+
+#[wasm_bindgen]
+pub struct MatF32 {
+    inner: Mat<f32>
+}
+
 #[wasm_bindgen]
 pub fn greet() {
-    let mat = Mat::new(2, 2, vec![
-        1.0, 0.0,
-        3.0, 1.0,
-    ]);
     let window = web_sys::window().expect("uh oh");
     window.alert_with_message("Hello, wasm-algeo-demos!").expect("oh no");
 }
--- www/src/index.js
+++ www/src/index.js
@@ -5,29 +5,70 @@ import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/eclipse.css';
 
-class Demo {
+// CSS
+import "./style.css";
+
+class Main {
 
 	constructor() {
+		// wasm.greet();
+
 		this.codeMirror = CodeMirror(
 			document.getElementById("codeMirror"),
 			{
 				value: 
-`function myScript(){
-	return 100;
+`// create a demo
+function create_demo(context){
+
+	// initialize ui/controller elements
+	let lowerBound = 0;
+	let upperBound = 10;
+	let step = 1;
+
+	let n = context.addSlider("n", lowerBound, upperBound, step)
+	let disp1 = context.addLatexDisplay();
+	let rend = context.add2DRenderer();
+
+	// set "run" method
+	context.setRun(()=>{
+		// create polynomial x^n
+		let poly = Poly("x").pow(n.inner);
+
+		disp1.display(poly);
+		rend.display(poly);
+	})
 }`,
 				mode:  "javascript",
-				theme: "default"
+				theme: "default",
+				viewportMargin: Infinity,
 			}
 		);
+
+		document
+			.getElementById("createDemo")
+			.addEventListener(
+				"click",
+				()=>this.createDemo()
+			);
+
+
 	}
 
+	createDemo() {
+		let context = new Context();
+
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#never_use_eval!
+		Function(
+			'"use strict"; return (' + this.codeMirror.getValue() + ')'
+		)()(context);
+	}
 }
 
-function main() {
-	//wasm.greet();
-	new Demo();
+class Context {
+	constructor() {
+
+	}
 }
 
 
-
-main();
+new Main();
