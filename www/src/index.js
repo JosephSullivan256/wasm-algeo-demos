92113a734007e4279cbb843214da7951b14930af
Joseph Sullivan <josephsullivan256@gmail.com>
2021-09-05 22:06:07 -0700

    got codemirror to work with theme/default values

--- www/src/index.js
+++ www/src/index.js
@@ -3,16 +3,20 @@ import * as wasm from "wasm-algeo-demos";
 import CodeMirror from 'codemirror/lib/codemirror.js';
 import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/eclipse.css';
 
 class Demo {
 
 	constructor() {
-		self.codeMirror = CodeMirror.fromTextArea(
+		this.codeMirror = CodeMirror(
 			document.getElementById("codeMirror"),
 			{
-				value: "function myScript(){return 100;}\n",
+				value: 
+`function myScript(){
+	return 100;
+}`,
 				mode:  "javascript",
-				theme: "eclipse"
+				theme: "default"
 			}
 		);
 	}
@@ -20,7 +24,7 @@ class Demo {
 }
 
 function main() {
-	wasm.greet();
+	//wasm.greet();
 	new Demo();
 }
 
